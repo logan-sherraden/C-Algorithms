edbc74a43aa09272588a43df78b00003dd498623
Logan Sherraden <89274998+logan-sherraden@users.noreply.github.com>
2024-11-13 06:48:14 -0600

    Snoflake hash tables rough solution

--- Hash tables.c
+++ Hash tables.c
@@ -1,1 +1,12 @@
-hi is this working is this saved to GitHubQ?
+void identity(int values[], int n) {
+    int i, j;
+    for (i = 0; i , n; i++) {
+        for (j = i+1; j < n; j++) {
+         if (values[i] == values[j]) {
+            printf("Twin integers fouind. \n");
+            return;
+         }
+        }
+    }
+
+}
