5215a81ab3d88772a71f9c37de28e9861300273a
Logan Sherraden <89274998+logan-sherraden@users.noreply.github.com>
2024-11-19 10:05:40 -0600

    added first scanning function

--- hashTables/Inputs/hash_tables.c
+++ hashTables/Inputs/hash_tables.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-void identify_identical(int values[], int n) {
+// snowflakes are represented as arrays with each number representing the length of each snowflakes arms
+// identify identical arrays function
+void identify_identical(int arr[], int n) {
     int i, j;
     for (i = 0; i < n; i++) {
         for (j = i+1; j < n; j++) {
-            if (values[i] == values[j]) {
+            if  (arr[i] == arr[j]) {
                 printf("Twin integers found.\n");
                 return;
             }
@@ -18,3 +20,16 @@ int main(void) {
     identify_identical(a, 5);
     return 0;
 }
+
+// scan arrays from right to left
+int identical_right(int snow1[], int snow2[],
+                    int start) {
+    int offset;
+    for (offset =0; offset < 6; offset++) {
+    if (snow1[offset] != snow2[start + offset]) 
+        return 0;
+    }
+    return 1;
+ }
+
+// scan arrays from left to right
